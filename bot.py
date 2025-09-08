b7592b3710b90267f58ad4b0a3f59f3dc5a583c0
tabahi59 <115520568+tabahi59@users.noreply.github.com>
2025-09-08 08:33:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,7 @@
+import time
+
 print("🚀 Insta Bot is running successfully on Render!")
+
+while True:
+    print("🤖 Bot abhi bhi chal raha hai...")
+    time.sleep(30)  # 30 sec ke baad fir check karega
